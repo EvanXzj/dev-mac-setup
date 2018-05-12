aa1c0f20948ae924a835bc6b77b11cbce3d2a8ec
EvanXzj <2686465170@qq.com>
2018-05-12 15:01:57 +0800

    update vim setup shell file

--- vimrcs/basic.sh
+++ vimrcs/basic.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_basic_vimrc.sh
+# git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+# sh ~/.vim_runtime/install_basic_vimrc.sh
+
+cat ~/.config_runtime/vimrcs/basic.vim > ~/.vimrc
+
+echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
