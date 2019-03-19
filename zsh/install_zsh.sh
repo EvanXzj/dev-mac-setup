8dffb95a4deb01b213c688ccbb53a9e881f96a49
EvanXzj <2686465170@qq.com>
2019-03-19 15:51:26 +0800

    add zsh install shell script

---
+++ zsh/install_zsh.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+# sh ~/.vim_runtime/install_basic_vimrc.sh
+
+cat ~/.config_runtime/zsh/.zshrc > ~/.zshrc
+
+echo "Installed the Basic zsh configuration successfully! Enjoy :-)"
